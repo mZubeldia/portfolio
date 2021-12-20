4968e39758f826e958a79cc6e33fe4224211b4dd
María Zubeldia <mzubeldia09@gmail.com>
2021-12-20 21:19:20 +0100

    create home component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
-      <main class="main main_landing">
-        <div class="text-wrapper">
-          <h2 class="main_text">
-            <span class="main_text_span greeting">Hi there, I'm María</span>
-            <span class="main_text_span job">a Frontend Developer</span>
-          </h2>
-        </div>
-        <div class="image-wrapper"></div>
-      </main>
+      <Home />
       <Footer />
     </>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <main class="main main_landing">
+      <div class="text-wrapper">
+        <h2 class="main_text">
+          <span class="main_text_span greeting">Hi there, I'm María</span>
+          <span class="main_text_span job">a Frontend Developer</span>
+        </h2>
+      </div>
+      <div class="image-wrapper"></div>
+    </main>
+  );
+};
+export default Home;

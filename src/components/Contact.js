b75383dc3e8422e9909c4473f373cc27280d4ee1
María Zubeldia <mzubeldia09@gmail.com>
2021-12-23 17:42:46 +0100

    change for in form for htmlfor

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,15 @@ const Contact = () => {
         <h2 className="page_title">Contact me</h2>
           <form action="https://formspree.io/f/xjvlvooa" method="POST" className="form_contact">
             <div className="form-group col-md-12">
-              <label for="name">Full Name</label>
+              <label htmlFor="name">Full Name</label>
               <input required name="name" type="text" className="form-control" id="name" placeholder="John Smith" />
             </div>
             <div className="form-group col-md-12">
-              <label for="email">Email</label>
+              <label htmlFor="email">Email</label>
               <input required name="email" type="email" className="form-control" id="email" placeholder="johnsmith@example.com"/>
             </div>
             <div className="form-group col-md-12">
-              <label for="message">Reasons</label>
+              <label htmlFor="message">Reasons</label>
               <textarea required name="message" type="text" rows="4" className="form-control"  id="message" placeholder="I want to work with you!"></textarea>
             </div>
             <button className="myButton">Submit</button>

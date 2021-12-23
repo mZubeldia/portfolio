afda066b04cef9b687ab7cfe162cecba1d7b1bf4
María Zubeldia <mzubeldia09@gmail.com>
2021-12-23 12:25:59 +0100

    create projects componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 //import logo from './logo.svg';
 import "./App.css";
-import Contact from "./components/Contact";
 
 
 //componenets
-import Curriculum from "./components/Curriculum";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+
+import Curriculum from "./components/Curriculum";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
       <Header />
       <Home />
       <Curriculum />
+      <Projects />
       <Contact />
       <Footer />
     </>
